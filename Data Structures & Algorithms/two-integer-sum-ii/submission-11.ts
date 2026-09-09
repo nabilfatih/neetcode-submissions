34d98cdd7903e46f0df32516e42e5fbb333bd182
Nabil Fatih <77352432+nabilfatih@users.noreply.github.com>
2026-09-09 11:32:07 +0200

    Add: two-integer-sum-ii - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-11.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers: number[], target: number): number[] {
+        let left = 0;
+        let right = numbers.length - 1;
+
+        if (numbers[left] + numbers[right] === target) {
+            return [left + 1, right + 1];
+        }
+
+        while (left < right) {
+            if (numbers[left] + numbers[right] > target) {
+                right--;
+                continue;
+            }
+
+            if (numbers[left] + numbers[right] < target) {
+                left++;
+                continue;
+            }
+
+            return [left + 1, right + 1];
+        }
+    }
+}
