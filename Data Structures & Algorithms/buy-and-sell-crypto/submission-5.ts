efe0a4a56aebfa99d7934ab771aea38716e09149
Nabil Fatih <77352432+nabilfatih@users.noreply.github.com>
2026-09-09 18:44:34 +0200

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let maxProfit = 0
+        let minPrice = prices[0]
+
+        for(let i = 1; i < prices.length; i++) {
+            const profit = prices[i] - minPrice
+            maxProfit = Math.max(maxProfit, profit)
+            minPrice = Math.min(minPrice, prices[i])
+        }
+
+        return maxProfit
+    }
+}
