2a99acad3aaf7daac021c7a82c3f58051ec18add
Nabil Fatih <77352432+nabilfatih@users.noreply.github.com>
2026-09-10 22:42:35 +0200

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.ts
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens: string[]): number {
+        const stack = []; // stack will always has length 2
+
+        for (const value of tokens) {
+            const num = parseInt(value)
+            if (!Number.isNaN(num)) {
+                stack.push(num);
+            } else {
+                if (value === "+") {
+                    stack.push(stack.pop() + stack.pop());
+                }
+                if (value === "-") {
+                    const a = stack.pop();
+                    const b = stack.pop();
+
+                    stack.push(b - a);
+                }
+                if (value === "*") {
+                    stack.push(stack.pop() * stack.pop());
+                }
+                if (value === "/") {
+                    const a = stack.pop();
+                    const b = stack.pop();
+
+                    stack.push(Math.trunc(b / a));
+                }
+            }
+        }
+
+        return stack[0];
+    }
+}
