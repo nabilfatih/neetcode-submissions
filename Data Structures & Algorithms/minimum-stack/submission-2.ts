e17bfa2eb3fbc63918f7096e58e81578ae9b3b5a
Nabil Fatih <77352432+nabilfatih@users.noreply.github.com>
2026-09-10 22:10:46 +0200

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.ts
@@ -0,0 +1,45 @@
+class MinStack {
+    readonly stack: number[] = [];
+    readonly minStack: number[] = [];
+    constructor() {
+        this.stack;
+        this.minStack;
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.stack.push(val);
+
+        let min = Math.min(
+            val,
+            this.minStack.length === 0 ? val : this.minStack[this.minStack.length - 1],
+        );
+
+        this.minStack.push(min)
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
